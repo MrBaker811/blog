33554358e7eb05df2a84ae955cb2cba6cda3f82b
Sean Baker <musiq365@yahoo.com>
2014-09-16 14:05:08 -0400

    custom and styles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
  belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "500x2000>", :large => "1140x2000>" }
  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 
