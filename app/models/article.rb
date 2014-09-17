54023c0ff29ec0934d117a167fcc0173d19d479f
Sean Baker <musiq365@yahoo.com>
2014-09-17 15:09:51 -0400

    alot of customizing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,13 @@
 class Article < ActiveRecord::Base
  belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "500x2000>", :large => "1140x2000>" }
+  has_attached_file :photo, :styles => { :medium => "500x500>", :large => "500x2000>" }
  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 
   
 
 	
-
+validates :photo, presence: true
 validates :title, presence: true,
                     length: { minimum: 5 }
 end
